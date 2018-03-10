6cbf4dd371475528298d00e3d9031d9bb2c5b77c
sonorks <julivas96@gmail.com>
2018-03-10 08:28:46 -0500

    Base

---
+++ dojo-spring/src/main/java/co/edu/udea/dojospring/controller/PostController.java
@@ -0,0 +1,19 @@
+package co.edu.udea.dojospring.controller;
+
+
+import co.edu.udea.dojospring.exception.ResourceNotFoundException;
+import co.edu.udea.dojospring.model.Post;
+import co.edu.udea.dojospring.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+public class PostController{
+
+}
---
+++ dojo-spring/src/main/java/co/edu/udea/dojospring/model/Post.java
@@ -0,0 +1,64 @@
+package co.edu.udea.dojospring.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+public class Post {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
